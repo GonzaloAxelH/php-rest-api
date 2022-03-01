0b22419b87b863095a3343f1d2151e4c1b7e1994
GonzaloAxelH <gonzaloaxelh@gmail.com>
2022-02-28 19:59:54 -0500

    Iniciando y configurando rutas

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+//RUTA RESET
+Route::post("/reset", "ResetController@reset");
+//CREAR UNA CUENTA CON BALANCE INICIAL
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//RUTA PARA OBTENER UNA BALANCE DE CUENTA EXISTENTE
+Route::get("/balance/{id}", "BalanceController@getBalance");
+//RUTAS PARA TRANFERENCIAS
+Route::post("/event", "EventController@createEvent");
